7908436f5f7b0c23db2170744d3c7eed451d4df6
hussain <hussainabdullatif@marun.edu.tr>
2023-12-14 01:00:21 +0300

    Create auth slice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
 
 function App() {
+    const isAuth = useSelector((state) => state.auth.isAuth);
+
     return (
         <div className="App">
-            <Auth />
-            {/* <Layout /> */}
+            {!isAuth && <Auth />}
+            {isAuth && <Layout />}
         </div>
     );
 }
