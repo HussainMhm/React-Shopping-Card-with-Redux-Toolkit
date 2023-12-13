be72d8cd28035fd0348b68f537d302a58488fb14
hussain <hussainabdullatif@marun.edu.tr>
2023-12-14 00:49:17 +0300

    Initialize Shopping Card Project Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { actions } from "./store/index";
+import React from "react";
 import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
 
 function App() {
-    const dispatch = useDispatch();
-    const counter = useSelector((state) => state.counter);
-
-    const incrementHandler = () => {
-        dispatch(actions.increment());
-    };
-
-    const decrementHandler = () => {
-        dispatch(actions.decrement());
-    };
-
     return (
         <div className="App">
-            <h1>Counter</h1>
-            <h2>{counter}</h2>
-
-            <button onClick={incrementHandler}>Increment</button>
-            <button onClick={decrementHandler}>Decrement</button>
+            <Auth />
+            {/* <Layout /> */}
         </div>
     );
 }
