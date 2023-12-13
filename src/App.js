126940b22914411d66e039bf343067e439ff1220
hussain <hussainabdullatif@marun.edu.tr>
2023-12-14 02:48:42 +0300

    Sending asynchronous HTTP Requests with Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-import { useSelector } from "react-redux";
+import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+    const dispatch = useDispatch();
+    const notification = useSelector((state) => state.ui.notification);
     const isAuth = useSelector((state) => state.auth.isAuth);
+    const cart = useSelector((state) => state.cart);
 
-    const cartItems = useSelector((state) => state.cart.items);
+    useEffect(() => {
+        const sendRequest = async () => {
+            const response = await fetch(
+                "https://redux-trial-3b6ba-default-rtdb.firebaseio.com/cartitems.json",
+                {
+                    method: "PUT",
+                    body: JSON.stringify(cart),
+                }
+            );
+            const data = await response.json();
+        };
 
-    return <div className="App">{isAuth ? <Layout /> : <Auth />}</div>;
+        sendRequest();
+    });
+
+    return (
+        <div className="App">
+            {notification && (
+                <Notification type={notification.type} message={notification.message} />
+            )}
+            {isAuth ? <Layout /> : <Auth />}
+        </div>
+    );
 }
 
 export default App;
